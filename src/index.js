49703468229ae88491571b75121d441dc16db392
Vyacheslav lulisFF <slavacssworld@gmail.com>
2023-08-14 23:29:49 +0600

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const bracketsMap = new Map(bracketsConfig);
+  const stack = [];
+
+  for (const symbol of str) {
+    if (bracketsMap.get(stack[stack.length - 1]) == symbol) {
+      stack.pop();
+    } else {
+      stack.push(symbol);
+    }
+  }
+  return stack.length === 0;
+};
